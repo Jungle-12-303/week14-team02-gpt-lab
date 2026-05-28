1084ab2d59864cb68f263b50cb33bbad7d4e2779
ummfieg <ummfieg@naver.com>
2026-05-28 16:17:50 +0900

    feat: 해당 sample가져오는 getitem 함수 구현

--- src/dataset.py
+++ src/dataset.py
@@ -30,7 +30,7 @@ class GPTDataset(Dataset):
         #start의 최대 시작점
         max_start_index = len(self.token_ids) - self.context_length - 1
 
-        # 최대 시작점이 0보다 작으면면 0으로 설정
+        # 최대 시작점이 0보다 작으면 0으로 설정
         if max_start_index < 0:
             self._length = 0
 
@@ -46,13 +46,26 @@ class GPTDataset(Dataset):
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         """
         TODO: idx번째 input_ids와 target_ids를 LongTensor로 반환합니다.
-
+        
         Returns:
             input_ids: (context_length,)
             target_ids: (context_length,)
         """
-        raise NotImplementedError("GPTDataset.__getitem__을 구현하세요.")
+        # sample의 시작 index구하기
+        start = idx * self.stride
+        # 시작점에서 자를 길이를 더함
+        end = start + self.context_length
+        
+        # 입력값 만들기
+        input_ids = self.token_ids[start:end]
+        # 입력값 보다 한 칸 뒤의 정답 target 만들기
+        target_ids = self.token_ids[start + 1: end + 1]
 
+        # tensor로 바꿔서 반환
+        return (
+        torch.tensor(input_ids, dtype=torch.long),
+        torch.tensor(target_ids, dtype=torch.long),
+        )
 
 def create_dataloader(
     token_ids: list[int],
@@ -64,4 +77,4 @@ def create_dataloader(
     num_workers: int = 0,
 ) -> DataLoader:
     """TODO: GPTDataset을 만들고 torch.utils.data.DataLoader로 감싸 반환합니다."""
-    raise NotImplementedError("create_dataloader를 구현하세요.")
+
