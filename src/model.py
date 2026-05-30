b7f1b5f3c308c6d7b65b9712ceb8b602a288b35f
ummfieg <ummfieg@naver.com>
2026-05-30 18:31:05 +0900

    feat: transformerblock init 설정

--- src/model.py
+++ src/model.py
@@ -84,7 +84,25 @@ class TransformerBlock(nn.Module):
     ):
         super().__init__()
         # TODO: attention, ffn, layernorm, dropout을 정의하세요.
-        raise NotImplementedError("TransformerBlock.__init__을 구현하세요.")
+        # attention 정의
+        self.attention = MultiHeadAttention(
+            d_model=d_model,
+            n_heads=n_heads,
+            drop_rate=drop_rate,
+            qkv_bias=qkv_bias,
+        )
+        # feedforward 정의
+        self.ffn = FeedForward(
+            d_model=d_model,
+            dropout=drop_rate
+        )
+
+        # layerNorm 정의
+        self.layerNorm1 = LayerNorm(d_model)
+        self.layerNorm2 = LayerNorm(d_model)
+
+        # dropout 정의
+        self.drop = nn.Dropout(drop_rate)
 
     def forward(self, x: torch.Tensor, causal_mask: bool = True) -> torch.Tensor:
         """TODO: attention과 ffn을 residual connection으로 연결합니다."""
